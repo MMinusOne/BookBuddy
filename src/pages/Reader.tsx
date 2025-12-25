0842a174d3401f9c022c5185cc0df83b8a5b0f54
MMinusOne <michael.business.lb@proton.me>
2025-12-25 19:41:54 +0200

    some bug fixes

--- src/pages/Reader.tsx
+++ src/pages/Reader.tsx
@@ -5,7 +5,7 @@ import usePage from "../lib/state/pageState";
 
 import "react-pdf/dist/Page/TextLayer.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Book } from "../lib/Book";
 import { morphBook } from "../lib/services/morphBook";
 import { FaArrowDown, FaArrowUp, FaRobot } from "react-icons/fa";
@@ -23,9 +23,6 @@ export default function ReaderView() {
     return <></>;
   }
 
-  const [visiblePages, setVisiblePages] = useState(
-    new Set([currentBook.current_page + 1])
-  );
   const pageRefs = useRef<(HTMLDivElement | null)[]>([]);
   const documentContainerRef = useRef<HTMLDivElement>(null);
   const [readerState, setReaderState] = useState<{
@@ -40,6 +37,12 @@ export default function ReaderView() {
     bookData: currentBook,
   });
   const pdfRef = useRef<HTMLDivElement>(null);
+  const [numPagesLoaded, setNumPagesLoaded] = useState(0);
+  const allPagesLoaded = useMemo(() => {
+    return (
+      currentBook.page_count > 0 && numPagesLoaded == currentBook.page_count
+    );
+  }, [numPagesLoaded, currentBook.page_count]);
 
   const setPageRef = useCallback(
     (pageIndex: number) => (el: HTMLDivElement | null) => {
@@ -70,34 +73,36 @@ export default function ReaderView() {
             entry.target.getAttribute("data-page") || "0"
           );
 
-          setVisiblePages((prev) => {
-            const newSet = new Set(prev);
-            if (entry.isIntersecting) {
-              [0, -1, +1].forEach((offset) => newSet.add(pageNumber + offset));
-              setReaderState((prev) => {
-                const newBookData = new Book(prev.bookData);
-                newBookData.setCurrentPage(pageNumber);
-                return { ...prev, bookData: newBookData };
-              });
-            } else {
-              newSet.delete(pageNumber);
-            }
-
-            return newSet;
-          });
+          if (entry.isIntersecting) {
+            setReaderState((prev) => {
+              const newBookData = new Book(prev.bookData);
+              newBookData.setCurrentPage(pageNumber);
+              return { ...prev, bookData: newBookData };
+            });
+          }
         }
       },
       { root: null, rootMargin: "500px", threshold: 0 }
     );
-
     pageRefs.current.forEach((pageRef) => {
       if (pageRef) observer.observe(pageRef);
     });
-    const currentPage = pageRefs.current[currentBook.current_page];
+
+    return () => observer.disconnect();
+  }, [isLoading, readerState.bookData.current_page]);
+
+  useEffect(() => {
+    setIsLoading(true);
+  }, []);
+
+  useEffect(() => {
+    if (!allPagesLoaded) return;
+
+    const currentPageIndex = currentBook.current_page - 1;
+    const currentPage = pageRefs.current[currentPageIndex];
 
     if (documentContainerRef.current && currentPage) {
       const pageRect = currentPage.getBoundingClientRect();
-
       const containerRect =
         documentContainerRef.current.getBoundingClientRect();
 
@@ -106,18 +111,14 @@ export default function ReaderView() {
         containerRect.top +
         documentContainerRef.current.scrollTop;
 
+      console.log(scrollTop);
+
       documentContainerRef.current.scrollTo({
         behavior: "instant",
         top: scrollTop,
       });
     }
-
-    return () => observer.disconnect();
-  }, [isLoading]);
-
-  useEffect(() => {
-    setIsLoading(true);
-  }, []);
+  }, [isLoading, allPagesLoaded]);
 
   useEffect(() => {
     (async () => {
@@ -127,82 +128,112 @@ export default function ReaderView() {
 
   return (
     <>
-      <div className="w-full h-full flex flex-col">
+      <div className="flex flex-col w-full h-full">
         <Header />
 
         <div className="flex w-full h-full overflow-hidden">
           <div
             ref={documentContainerRef}
-            className="w-full h-full flex flex-col items-center justify-start overflow-hidden overflow-y-scroll"
+            style={{
+              overflowY: allPagesLoaded ? "scroll" : "hidden",
+            }}
+            className="flex flex-col justify-start items-center w-full h-full overflow-hidden"
           >
             <div ref={pdfRef} style={{ userSelect: "text" }}>
               <Document
-                className="w-fit flex flex-col gap-2"
+                className="flex flex-col gap-2 w-fit"
                 onLoadSuccess={() => {
                   setIsLoading(false);
                 }}
+                loading={<>Loading</>}
                 file={convertFileSrc(currentBook?.book_path)}
               >
                 {new Array(currentBook.page_count)
                   .fill(0)
                   .map((_, pageIndex) => {
                     const pageNumber = pageIndex + 1;
-                    const shouldRender = visiblePages.has(pageNumber);
 
                     return (
                       <div
                         key={pageIndex}
                         ref={setPageRef(pageIndex)}
                         data-page={pageNumber}
-                        style={{ minHeight: shouldRender ? "auto" : "1100px" }}
                       >
-                        {shouldRender ? (
-                          <Page
-                            renderTextLayer={true}
-                            renderAnnotationLayer={true}
-                            width={800}
-                            pageIndex={pageIndex}
-                          />
-                        ) : (
-                          <div className="w-full h-full flex items-center justify-center">
-                            <span className="badge">Page {pageNumber}</span>
-                          </div>
-                        )}
+                        <Page
+                          renderTextLayer={true}
+                          renderAnnotationLayer={true}
+                          width={800 * (readerState.zoom / 100)}
+                          pageIndex={pageIndex}
+                          onRenderSuccess={() => {
+                            setNumPagesLoaded((prev) => prev + 1);
+                          }}
+                        />
                       </div>
                     );
                   })}
               </Document>
             </div>
           </div>
-          <div className="h-full w-12.5 bg-base-300 flex p-1 flex-col items-center justify-between">
-            <div className="my-2 flex items-center flex-col">
-              <button className="w-full btn aspect-square">
+          <div className="flex flex-col justify-between items-center bg-base-300 p-1 w-12.5 h-full">
+            <div className="flex flex-col items-center my-2">
+              <button className="w-full aspect-square btn btn-ghost">
                 <FaRobot />
               </button>
             </div>
 
-            <div className="my-2 flex items-center flex-col">
-              <span className="flex items-center justify-center aspect-square w-full text-sm">
+            <div className="flex flex-col items-center my-2">
+              <span className="flex justify-center items-center w-full aspect-square text-sm">
                 {readerState.bookData.current_page}
               </span>
 
-              <span className="flex items-center justify-center aspect-square w-full text-sm">
+              <span className="flex justify-center items-center w-full aspect-square text-sm">
                 {readerState.bookData.page_count}
               </span>
 
-              <button className="btn aspect-square">
+              <button
+                onClick={() => {
+                  setReaderState((prev) => {
+                    const newBook = new Book(prev.bookData);
+                    newBook.current_page += 1;
+                    return { ...prev, bookData: newBook };
+                  });
+                }}
+                className="aspect-square btn btn-ghost"
+              >
                 <FaArrowUp />
               </button>
+              <button
+                onClick={() => {
+                  setReaderState((prev) => {
+                    const newBook = new Book(prev.bookData);
+                    newBook.current_page -= 1;
+                    return { ...prev, bookData: newBook };
+                  });
+                }}
+                className="aspect-square btn btn-ghost"
+              >
+                <FaArrowDown />
+              </button>
 
-              <span className="flex items-center justify-center aspect-square w-full text-sm">
+              <span className="flex justify-center items-center w-full aspect-square text-sm">
                 {readerState.zoom}%
               </span>
 
-              <button className="btn aspect-square">
+              <button
+                onClick={() => {
+                  setReaderState((prev) => ({ ...prev, zoom: prev.zoom + 10 }));
+                }}
+                className="aspect-square btn btn-ghost"
+              >
                 <FaMagnifyingGlassPlus />
               </button>
 
-              <button className=" btn btn-ghost aspect-square">
+              <button
+                onClick={() => {
+                  setReaderState((prev) => ({ ...prev, zoom: prev.zoom - 10 }));
+                }}
+                className="aspect-square btn btn-ghost"
+              >
                 <FaMagnifyingGlassMinus />
               </button>
             </div>
